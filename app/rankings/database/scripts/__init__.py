bdd8c5f223ad8c83851897a58cb787ac68d8b643
Henry Cagnini <henry.cagnini@gmail.com>
2025-10-14 19:58:49 -0300

    supposedly fixed duplication error

--- app/rankings/database/scripts/__init__.py
+++ app/rankings/database/scripts/__init__.py
@@ -1,7 +1,9 @@
 import os
 
 import pandas as pd
+from django.db.models import Min, Subquery
 from tqdm import tqdm
+from django.db.models.functions import Lower
 
 try:
     from ..rankings.models import Universidade, ApelidoDeUniversidade, ApelidoDePais, IES, UltimaCarga
@@ -39,24 +41,30 @@ def populate_brazilian_universities() -> dict:
     ):
         rows = gb.get_group((cod_ies, ))
 
-        universidade = Universidade(
-            nome_portugues=rows.iloc[0]['Universidade'],
-            nome_ingles=rows.iloc[0]['Universidade'],
-            cod_ies=cod_ies,
-            pais_apelido_id=id_apelido_pais
-        )
-        universidade.save()
-
-        count_unis += 1
+        try:
+            universidade = Universidade.objects.get(cod_ies=cod_ies)
+        except Universidade.DoesNotExist:
+            universidade = Universidade(
+                nome_portugues=rows.iloc[0]['Universidade'],
+                nome_ingles=rows.iloc[0]['Universidade'],
+                cod_ies=cod_ies,
+                pais_apelido_id=id_apelido_pais
+            )
+            universidade.save()
+            count_unis += 1
 
         apelidos = []
         for i, row in rows.iterrows():
-            apelidos += [
-                ApelidoDeUniversidade(
-                    universidade=universidade,
-                    apelido=row['Universidade']
-                )
-            ]
+            try:
+                # tenta achar no banco; se achar, não insere!
+                apelido = ApelidoDeUniversidade.objects.get(apelido__iexact=row['Universidade'].lower())
+            except ApelidoDeUniversidade.DoesNotExist:
+                apelidos += [
+                    ApelidoDeUniversidade(
+                        universidade=universidade,
+                        apelido=row['Universidade']
+                    )
+                ]
 
         ApelidoDeUniversidade.objects.bulk_create(apelidos)
         count_apelidos += len(apelidos)
